10569712bef9c1ad821a56d60a9abf3560efd82f
SunDoge <384813529@qq.com>
2018-07-19 15:38:03 +0800

    dag

--- src/trie.rs
+++ src/trie.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct DictUnit {
     word: char,
     weight: f64,
@@ -52,14 +52,19 @@ impl TrieNode {
             .entry(key[0])
             .or_insert(TrieNode::new())
             .insert(&key[1..], value);
-        
     }
 
     pub fn find(&self, key: &[char]) -> Option<&DictUnit> {
-        match key.len() {
-            0 => self.value.as_ref(),
-            _ => self.children.get(&key[0]).and_then(|c| c.find(&key[1..])),
+        // match key.len() {
+        //     0 => self.value.as_ref(),
+        //     _ => self.children.get(&key[0]).and_then(|c| c.find(&key[1..])),
+        // }
+        if key.is_empty() {
+            return self.value.as_ref();
         }
+        self.children
+            .get(&key[0])
+            .and_then(|child| child.find(&key[1..]))
     }
 }
 
@@ -87,6 +92,16 @@ impl Trie {
         let chars: Vec<char> = key.chars().collect();
         self.root.insert(&chars, value);
     }
+
+    pub fn find(&self, key: &str) -> Option<&DictUnit> {
+        if key.is_empty() {
+            return None;
+        }
+        let chars: Vec<char> = key.chars().collect();
+        self.root.find(&chars)
+    }
+
+    pub fn find_dag(&self, key: &str, res: &Vec<Dag>) {}
 }
 
 #[cfg(test)]
@@ -96,23 +111,25 @@ mod tests {
     #[test]
     fn trie() {
         let mut t = Trie::new();
-        t.insert(
-            "我们",
-            DictUnit {
-                word: 'w',
-                weight: 0.1,
-                tag: "s".to_string(),
-            },
-        );
-        t.insert(
-            "我的",
-            DictUnit {
-                word: 'w',
-                weight: 0.1,
-                tag: "s".to_string(),
-            },
-        );
+        let v1 = DictUnit {
+            word: 'w',
+            weight: 0.1,
+            tag: "s".to_string(),
+        };
 
+        let v2 = DictUnit {
+            word: 'w',
+            weight: 0.1,
+            tag: "s".to_string(),
+        };
+
+        t.insert("我们", v1.clone());
+        t.insert("我的", v2.clone());
+
+        assert_eq!(&v1, t.find("我们").unwrap());
+        assert_eq!(&v2, t.find("我的").unwrap());
+        assert_eq!(None, t.find("我"));
+        assert_eq!(None, t.find("我没有"));
         println!("{:#?}", t);
     }
 }
